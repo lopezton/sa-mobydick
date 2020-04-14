2fc44c4b7e5e4d22156e21c0ad3421d236c07463
Tony Lopez <tony.lopez1013@gmail.com>
2020-04-14 15:59:25 -0500

    adding starter files

---
+++ src/main/java/com/tonelope/demos/sa/mobydick/MobyDickApplication.java
@@ -0,0 +1,14 @@
+package com.tonelope.demos.sa.mobydick;
+
+/**
+ * A technical showcase.
+ *
+ * @author Tony Lopez
+ *
+ */
+public class MobyDickApplication {
+
+	public static void main(String[] args) {
+		
+	}
+}
