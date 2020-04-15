33bb240a775dba512479162028e0cc0b0ec50d72
Tony Lopez <tony.lopez1013@gmail.com>
2020-04-14 22:35:29 -0500

    adding WordCounter domain entity

---
+++ src/main/java/com/tonelope/demos/sa/mobydick/utils/WordCounter.java
@@ -0,0 +1,30 @@
+package com.tonelope.demos.sa.mobydick.utils;
+
+import java.util.Map;
+
+/**
+ *
+ *
+ * @author Tony Lopez
+ *
+ */
+public class WordCounter {
+
+	public WordCounter(String textFilename) {
+		// TODO
+	}
+	
+	public WordCounter(String textFilename, String stopFilename) {
+		// TODO
+	}
+	
+	public Map<String, Integer> getCountMap() {
+		// TODO
+		return null;
+	}
+	
+	public Integer getCount(String word) {
+		// TODO
+		return null;
+	}
+}
