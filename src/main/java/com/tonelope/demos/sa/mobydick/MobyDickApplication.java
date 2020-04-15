62a98b7323380027109b9ffc7ade38f6b446e805
Tony Lopez <tony.lopez1013@gmail.com>
2020-04-14 23:30:20 -0500

    adding executable logic

--- src/main/java/com/tonelope/demos/sa/mobydick/MobyDickApplication.java
+++ src/main/java/com/tonelope/demos/sa/mobydick/MobyDickApplication.java
@@ -1,5 +1,12 @@
 package com.tonelope.demos.sa.mobydick;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
+import com.tonelope.demos.sa.mobydick.utils.WordCounter;
+
 /**
  * A technical showcase.
  *
@@ -9,6 +16,22 @@ package com.tonelope.demos.sa.mobydick;
 public class MobyDickApplication {
 
 	public static void main(String[] args) {
-		
+		String textFilename = null;
+		String stopFilename = null;
+		if (null != args) {
+			if (args.length > 0) {
+				textFilename = args[0];
+			}
+			if (args.length > 2) {
+				stopFilename = args[1];
+			}
+		}
+
+		WordCounter wordCounter = new WordCounter(textFilename, stopFilename);
+
+		// get the map, stream it, sort it by value in descending order and print it
+		wordCounter.getCountMap().entrySet().stream()
+			.sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+			.forEach(e -> System.out.println(e.getKey() + ": " + e.getValue()));
 	}
 }
